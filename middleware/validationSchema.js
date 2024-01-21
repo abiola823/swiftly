8264324bf89ffb75bd46060122c96adbef19fddf
olaide <olaideabiola2@gmail.com>
2024-01-21 14:17:58 +0100

    edited register controller

--- middleware/validationSchema.js
+++ middleware/validationSchema.js
@@ -7,7 +7,7 @@ const registerSchema = (req, res) => {
             email: joi.string().email().required(),
             phone: joi.number().required(),
             password: joi.string().required().min(7).max(15),
-            referral: joi.string().optional()
+           
         });
         const {error} = registerValidationSchema.validate(req.body);
         if (error) return res.send(error.message);
